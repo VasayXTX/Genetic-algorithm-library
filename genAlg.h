993e328296eaa3cc537b97cee1e9c26bb40e4205
VasayXTX <VasayXTX@mail.ru>
2011-05-25 00:25:23 +1100

    Release class Population

--- genAlg.h
+++ genAlg.h
@@ -13,7 +13,7 @@ class Population
 {
 public:
     typedef std::pair<double, T> Elem;   //Element in population
-    typedef std::vector<Elem> Popul;
+    typedef std::vector<Elem *> Popul;
 
 private:
     unsigned int populSize, indSize, codeSize;
@@ -23,30 +23,27 @@ private:
 public:
     Population(unsigned int aPopulSize, unsigned int aIndSize, unsigned int aCodeSize, const F &aFuncFitness);
     Population(const Population &aPopup);
-    /*~Population();
+    ~Population();
 
-    void clear();*/
-    void generate();
+    void clear();
 
-    /*void insertPtr(unsigned int aIndex, PairIndividual *aPInd);
-    void insert(unsigned int aIndex, const PairIndividual &aPInd);
-    void replace(unsigned int aIndex, const PairIndividual &aPInd);
-
-    void mix(unsigned int aIterCount);
-    PairIndividual &getByIndex(unsigned int aIndex) const;
+    Population &operator =(const Population &aPopul);
 
     unsigned int getPopulSize() const;
     unsigned int getIndSize() const;
     unsigned int getCodeSize() const;
-    const F &getFuncFitness() const;
 
-    Population &operator =(const Population &aPopul);
+    void mix(unsigned int aIterCount);
 
-    PairIndividual *findRes() const;
+    void generate();
 
-    friend std::ostream &operator <<(std::ostream &aOut, const Population<F> &aPopul);
+    Elem *findBest() const;
+    Elem *getByIndex(unsigned int aIndex);
 
-    void print() const;*/
+    void insert(unsigned int aIndex, const T &aInd);
+    void insert(unsigned int aIndex, const typename Population<T, F>::Elem &aEl);
+
+    //void print() const;
 };
 
 template<typename T, typename F>
@@ -58,10 +55,7 @@ Population<T, F>::Population(unsigned int aPopulSize, unsigned int aIndSize, uns
 {
     popul.resize(populSize);
     for (unsigned int i = 0; i < populSize; ++i)
-    {
-        popul[i].second.setSize(aIndSize);
-        popul[i].second.setCodeSize(aCodeSize);
-    }
+        popul[i] = 0;
 }
 
 template<typename T, typename F>
@@ -71,14 +65,108 @@ Population<T, F>::Population(const Population &aPopup)
 }
 
 template<typename T, typename F>
-void Population<T, F>::generate()
+Population<T, F>::~Population()
+{
+    clear();
+}
+
+template<typename T, typename F>
+void Population<T, F>::clear()
 {
     for (Popul::iterator it = popul.begin(); it != popul.end(); ++it)
+        delete *it;
+}
+
+template<typename T, typename F>
+void Population<T, F>::generate()
+{
+    clear();
+    for (Popul::iterator it = popul.begin(); it != popul.end(); ++it)
     {
+        *it = new Elem();
+        (*it)->second.setSize(indSize);
+        (*it)->second.setCodeSize(codeSize);
         for (unsigned int i = 0; i < indSize; ++i)
-            (*it).second[i] = rand() % codeSize;
-        (*it).first = funcFitness((*it).second);
+        {
+            (*it)->second[i] = rand() % codeSize;
+        }
+        (*it)->first = funcFitness((*it)->second);
     }
+}
+
+template<typename T, typename F>
+typename Population<T, F> &Population<T, F>::operator =(const Population &aPopul)
+{
+    populSize = aPopul.populSize;
+    indSize = aPopul.indSize;
+    codeSize = aPopul.codeSize;
+    funcFitness = aPopul.funcFitness;
+    clear();
+    popul.resize(aPopul.populSize);
+    for (unsigned int i = 0; i < populSize; ++i)
+        popul[i] = new Elem(*aPopul.popul[i]);
+    return *this;
+}
+
+template<typename T, typename F>
+void Population<T, F>::mix(unsigned int aIterCount)
+{
+    for (unsigned int i = 0; i < aIterCount; ++i)
+    {
+        unsigned int index1 = rand() % populSize;
+        unsigned int index2 = rand() % populSize;
+        Population<T, F>::Elem *p = popul[index1];
+        popul[index1] = popul[index2];
+        popul[index2] = p;
+    }
+}
+
+template<typename T, typename F>
+unsigned int Population<T, F>::getPopulSize() const
+{
+    return populSize;
+}
+
+template<typename T, typename F>
+unsigned int Population<T, F>::getIndSize() const
+{
+    return indSize;
+}
+
+template<typename T, typename F>
+unsigned int Population<T, F>::getCodeSize() const
+{
+    return codeSize;
+}
+
+template<typename T, typename F>
+typename Population<T, F>::Elem *Population<T, F>::findBest() const
+{
+    Elem *res = popul[0];
+    for (unsigned int i = 1; i < populSize; ++i)
+    {
+        if (res->first > popul[i]->first)
+            res = popul[i];
+    }
+    return res;
+}
+
+template<typename T, typename F>
+void Population<T, F>::insert(unsigned int aIndex, const T &aInd)
+{
+    insert(aIndex, Elem(funcFitness(aInd), aInd));
+}
+
+template<typename T, typename F>
+void Population<T, F>::insert(unsigned int aIndex, const typename Population<T, F>::Elem &aEl)
+{
+    popul[aIndex] = new Elem(aEl);
+}
+
+template<typename T, typename F>
+typename Population<T, F>::Elem *Population<T, F>::getByIndex(unsigned int aIndex)
+{
+    return popul[aIndex];
 }
 
 #endif
